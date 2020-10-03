29c3fb5b355af88d2ab4c00632ca0aa741d2b7b1
phanikumaratyam <phanikumar.atyam@gmail.com>
2020-10-02 22:10:36 -0400

    added model classes, blank JSP files, and updated the skeleton code for controller class Purchase

--- FastFoodies/src/controller/Purchase.java
+++ FastFoodies/src/controller/Purchase.java
@@ -4,11 +4,50 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import model.Order;
+import model.PaymentInfo;
+import model.ShippingInfo;
 
 @Controller
 @RequestMapping("/purchase")
 public class Purchase {
-
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String viewOrderEntryForm(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return "OrderEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitItems", method = RequestMethod.POST)
+	public String submitItems(@ModelAttribute("order") Order order, HttpServletRequest request) {
+		request.getSession().setAttribute("order", order);
+		return "redirect:/purchase/paymentEntry";		
+	}
+	
+	@RequestMapping(path = "/paymentEntry", method = RequestMethod.GET)
+	public String viewPaymentEntryForm(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("payment", new PaymentInfo());
+		return "PaymentEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitPayment", method = RequestMethod.POST)
+	public String submitPayment(@ModelAttribute("payment") PaymentInfo payment, HttpServletRequest request) {
+		request.getSession().setAttribute("payment", payment);
+		return "redirect:/purchase/shippingEntry";	
+	}
+	
+	@RequestMapping(path = "/shippingEntry", method = RequestMethod.GET)
+	public String viewShippingEntryForm(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("shipping", new ShippingInfo());
+		return "ShippingEntryForm";
+	}
+	
+	@RequestMapping(path = "/submitShipping", method = RequestMethod.POST)
+	public String submitPayment(@ModelAttribute("shipping") ShippingInfo shipping, HttpServletRequest request) {
+		request.getSession().setAttribute("shipping", shipping);
+		return "redirect:/purchase/viewOrder";
+	}
 }
---
+++ FastFoodies/src/model/PaymentInfo.java
@@ -0,0 +1,40 @@
+package model;
+
+public class PaymentInfo {
+	private String creditCardNumber = null;
+	private String expirationDate = null;
+	private String cvvCode = null;
+	private String cardHolderName = null;
+	
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+	
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+	
+	public String getExpirationDate() {
+		return expirationDate;
+	}
+	
+	public void setExpirationDate(String expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+	
+	public String getCvvCode() {
+		return cvvCode;
+	}
+	
+	public void setCvvCode(String cvvCode) {
+		this.cvvCode = cvvCode;
+	}
+	
+	public String getCardHolderName() {
+		return cardHolderName;
+	}
+	
+	public void setCardHolderName(String cardHolderName) {
+		this.cardHolderName = cardHolderName;
+	}
+}
