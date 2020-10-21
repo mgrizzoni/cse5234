be2202f2f2db0578d7c69767ca2c233e00b89c4c
phanikumaratyam <phanikumar.atyam@gmail.com>
2020-10-21 19:34:37 -0400

    Added JPA facet and updated Item.java and InventoryServiceBean

--- FastFoodies-EJB/ejbModule/business/InventoryServiceBean.java
+++ FastFoodies-EJB/ejbModule/business/InventoryServiceBean.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import business.Inventory;
 import business.InventoryService;
@@ -18,7 +20,21 @@ public class InventoryServiceBean implements InventoryService {
     /**
      * Default constructor. 
      */
-    public InventoryServiceBean() {
+	
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	public static final String MY_QUERY= "Select i from Item i";
+	
+    public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public InventoryServiceBean() {
         // TODO Auto-generated constructor stub
     }
 
@@ -28,21 +44,23 @@ public class InventoryServiceBean implements InventoryService {
 		
 		Inventory inventory = new Inventory();
 		
-		List<Item> items = new ArrayList<Item>(5);
+		List<Item> items = new ArrayList<Item>();
+//		
+//		for(int i = 0; i < 5; i++) {
+//			items.add(new Item());
+//		}
+//		items.get(0).setName("Hamburger");
+//		items.get(0).setPrice("5.75");
+//		items.get(1).setName("Cheeseburger");
+//		items.get(1).setPrice("6.25");
+//		items.get(2).setName("French Fries");
+//		items.get(2).setPrice("1.75");
+//		items.get(3).setName("Coca-Cola");
+//		items.get(3).setPrice("1.00");
+//		items.get(4).setName("Sprite");
+//		items.get(4).setPrice("1.00");
 		
-		for(int i = 0; i < 5; i++) {
-			items.add(new Item());
-		}
-		items.get(0).setName("Hamburger");
-		items.get(0).setPrice("5.75");
-		items.get(1).setName("Cheeseburger");
-		items.get(1).setPrice("6.25");
-		items.get(2).setName("French Fries");
-		items.get(2).setPrice("1.75");
-		items.get(3).setName("Coca-Cola");
-		items.get(3).setPrice("1.00");
-		items.get(4).setName("Sprite");
-		items.get(4).setPrice("1.00");
+		items = entityManager.createQuery(MY_QUERY,Item.class).getResultList();
 		
 		inventory.setItems(items);
 		
