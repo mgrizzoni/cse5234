10c334a8617867fc3a4f0dc0b135f08256ad8a2c
phanikumaratyam <36789910+phanikumaratyam@users.noreply.github.com>
2020-11-02 00:01:33 -0500

    Updated the repository with Lab 8 Tasks

--- FastFoodies-EJB/ejbModule/business/InventoryServiceBean.java
+++ FastFoodies-EJB/ejbModule/business/InventoryServiceBean.java
@@ -7,9 +7,9 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-import business.Inventory;
-import business.InventoryService;
-import business.Item;
+import model.Item;
+
+
 
 /**
  * Session Bean implementation class InventoryServiceBean
@@ -45,20 +45,6 @@ public class InventoryServiceBean implements InventoryService {
 		Inventory inventory = new Inventory();
 		
 		List<Item> items = new ArrayList<Item>();
-//		
-//		for(int i = 0; i < 5; i++) {
-//			items.add(new Item());
-//		}
-//		items.get(0).setName("Hamburger");
-//		items.get(0).setPrice("5.75");
-//		items.get(1).setName("Cheeseburger");
-//		items.get(1).setPrice("6.25");
-//		items.get(2).setName("French Fries");
-//		items.get(2).setPrice("1.75");
-//		items.get(3).setName("Coca-Cola");
-//		items.get(3).setPrice("1.00");
-//		items.get(4).setName("Sprite");
-//		items.get(4).setPrice("1.00");
 		
 		items = entityManager.createQuery(MY_QUERY,Item.class).getResultList();
 		
